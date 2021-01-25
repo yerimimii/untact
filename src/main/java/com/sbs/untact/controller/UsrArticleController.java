fdd58cb61046385cbc592d5fdc721ed726e572b6
yerimimi <yerimimi@naver.com>
2021-01-25 23:33:57 +0900

    1일차 마무리

--- src/main/java/com/sbs/untact/controller/UsrArticleController.java
+++ src/main/java/com/sbs/untact/controller/UsrArticleController.java
@@ -1,7 +1,9 @@
 package com.sbs.untact.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,13 +13,16 @@ import com.sbs.untact.dto.Article;
 
 @Controller
 public class UsrArticleController {
+	private int articlesLastId;
 	private List<Article> articles;
 	
 	public UsrArticleController() {
+		//멤버변수 초기화 
+		articlesLastId = 0;
 		articles = new ArrayList<>();
-		articles.add(new Article(1, "2020-12-12 12:12:12", "제목1", "내용1"));
-		articles.add(new Article(2, "2020-12-12 12:12:12", "제목2", "내용2"));
 		
+		articles.add(new Article(++articlesLastId, "2020-12-12 12:12:12", "제목1", "내용1"));
+		articles.add(new Article(++articlesLastId, "2020-12-12 12:12:12", "제목2", "내용2"));
 	}
 	
 	@RequestMapping("/usr/article/detail")
@@ -31,5 +36,31 @@ public class UsrArticleController {
 	public List<Article> showList() {
 		return articles;
 	}
+	
+//	@RequestMapping("/usr/article/doAdd")
+//	@ResponseBody
+//	public Map<String, Object> doAdd(String regDate, String title, String body){
+//		articles.add(new Article(++articlesLastId, regDate, title, body));
+//		
+//		Map<String, Object> rs = new HashMap<>();
+//		rs.put("resultCode", "S-1");
+//		rs.put("msg", "성공하였습니다.");
+//		rs.put("id", articlesLastId);
+//		
+//		return rs;
+//	}
+//	
+//	@RequestMapping("/usr/article/doDelete")
+//	@ResponseBody
+//	public Map<String, Object> doAdd(String regDate, String title, String body){
+//		articles.add(new Article(++articlesLastId, regDate, title, body));
+//		
+//		Map<String, Object> rs = new HashMap<>();
+//		rs.put("resultCode", "S-1");
+//		rs.put("msg", "성공하였습니다.");
+//		rs.put("id", articlesLastId);
+//		
+//		return rs;
+//	}
 
 }
