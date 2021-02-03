be359a07fae23c9e87fe00d8da39adb90afc25e7
yerimimi <yerimimi@naver.com>
2021-02-03 21:32:43 +0900

    게시물수정

--- src/main/java/com/sbs/untact/controller/UsrArticleController.java
+++ src/main/java/com/sbs/untact/controller/UsrArticleController.java
@@ -15,72 +15,100 @@ import com.sbs.untact.dto.Article;
 public class UsrArticleController {
 	private int articlesLastId;
 	private List<Article> articles;
-	
+
 	public UsrArticleController() {
-		//멤버변수 초기화 
+		// 멤버변수 초기화
 		articlesLastId = 0;
 		articles = new ArrayList<>();
-		
+
 		articles.add(new Article(++articlesLastId, "2020-12-12 12:12:12", "제목1", "내용1"));
 		articles.add(new Article(++articlesLastId, "2020-12-12 12:12:12", "제목2", "내용2"));
 	}
-	
+
 	@RequestMapping("/usr/article/detail")
 	@ResponseBody
 	public Article showDetail(int id) {
 		return articles.get(id - 1);
-		//1번글 = index 0
+		// 1번글 = index 0
 	}
-	
+
 	@RequestMapping("/usr/article/list")
 	@ResponseBody
 	public List<Article> showList() {
 		return articles;
 	}
-	
+
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
-	public Map<String, Object> doAdd(String regDate, String title, String body){
+	public Map<String, Object> doAdd(String regDate, String title, String body) {
 		articles.add(new Article(++articlesLastId, regDate, title, body));
-		
+
 		Map<String, Object> rs = new HashMap<>();
 		rs.put("resultCode", "S-1");
 		rs.put("msg", "성공하였습니다.");
 		rs.put("id", articlesLastId);
-		
+
 		return rs;
 	}
-	
+
 	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
-	public Map<String, Object> doDelete(int id){
+	public Map<String, Object> doDelete(int id) {
 		boolean deleteArticleRs = deleteArticle(id);
 		articles.remove(id - 1);
-		
+
 		Map<String, Object> rs = new HashMap<>();
-		
-		if(deleteArticleRs) {
+
+		if (deleteArticleRs) {
 			rs.put("resultCode", "S-1");
 			rs.put("msg", "성공하였습니다.");
 		} else {
 			rs.put("resultCode", "F-1");
 			rs.put("msg", "해당 게시물이 존재하지 않습니다.");
 		}
-		
+
 		rs.put("id", id);
-		
+
 		return rs;
 	}
-	
+
 	private boolean deleteArticle(int id) {
 		for (Article article : articles) {
-			if(article.getId() == id) {
+			if (article.getId() == id) {
 				articles.remove(article);
 				return true;
 			}
 		}
-		
+
 		return false;
+	}
+
+	@RequestMapping("/usr/article/doModify")
+	@ResponseBody
+	public Map<String, Object> doModify(int id, String title, String body) {
+		Article selArticle = null;
+
+		for (Article article : articles) {
+			if (article.getId() == id) {
+				selArticle = article;
+				break;
+			}
+		}
+		
+		Map<String, Object> rs = new HashMap<>();
+		
+		if(selArticle == null) {
+			rs.put("resultCode", "F-1");
+			rs.put("msg", String.format("%d번 게시물은 존재하지 않습니다.", id));
+		}
+		selArticle.setTitle(title);
+		selArticle.setBody(body);
+
+		rs.put("resultCode", "S-1");
+		rs.put("msg", String.format("%d번 게시물이 수정되었습니다.", id));
+		rs.put("id", id);
+		
+		return rs;
 	}
 
 }
